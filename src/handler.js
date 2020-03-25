0812cf7da4c58232afa3acb8b8c51e2c550aa8f0
ChBLA <cbla19@student.aau.dk>
2020-03-25 14:59:40 +0100

    Handlers and testers
    Made a simpel server with handlers and a tester

---
+++ src/handler.js
@@ -0,0 +1,68 @@
+
+module.exports = {  
+    forkliftsHandler: forkliftsHandler,
+    routesHandler: routesHandler,
+    warehouseHandler: warehouseHandler,
+    ordersHandler: ordersHandler
+};
+
+
+function hasId(element) {
+    return typeof(element) === "string";
+}
+
+
+function forkliftsHandler(request, response) {
+    const id = hasId(request.parsedUrl[2]) ? request.parsedUrl[2] : null;
+
+    if (request.method === "GET") {
+        if (id !== null) {
+            console.log(`METHOD = ${request.method} (GET), id = ${id} (nnull)`);
+        } else {
+            console.log(`METHOD = ${request.method} (GET), id = ${id} (null)`);
+        }
+    } else if (request.method === "PUT" && id !== null) {
+        console.log(`METHOD = ${request.method} (PUT), id = ${id} (nnull)`);
+    } else if (request.method === "POST" && id !== null && request.parsedUrl[3] === "initiate") {
+        console.log(`METHOD = ${request.method} (POST), id = ${id} (nnull), initiate = ${request.parsedUrl[3]} (nnull)`);
+    } else {
+        console.log("error");
+    }
+
+}
+
+function routesHandler(request, response) {
+    if (request.method === "GET") {
+        console.log(`METHOD = ${request.method} (GET)`);
+    } else {
+        console.log("error");
+    }
+}
+
+function warehouseHandler(request, response) {
+    if (request.method === "GET") {
+        console.log(`METHOD = ${request.method} (GET)`);
+    } else if (request.method === "POST") {
+        console.log(`METHOD = ${request.method} (POST)`);
+    } else {
+        console.log("error");
+    }
+}
+
+function ordersHandler(request, response) {
+    const id = hasId(request.parsedUrl[2]) ? request.parsedUrl[2] : null;
+
+    if (request.method === "GET") {
+        if (id !== null) {
+            console.log(`METHOD = ${request.method} (GET), id = ${id} (nnull)`);
+        } else {
+            console.log(`METHOD = ${request.method} (GET), id = ${id} (null)`);
+        }
+    } else if (request.method === "POST") {
+        console.log(`METHOD = ${request.method} (POST)`);
+    } else {
+        console.log("error");
+    }
+
+}
+
