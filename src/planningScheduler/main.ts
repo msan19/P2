ff317a860acc1fb46df919f113e894ed061e82aa
HGinnerup <hginnerup@hotmail.com>
2020-03-30 13:07:20 +0200

    Cleaned up main

--- src/planningScheduler/main.ts
+++ src/planningScheduler/main.ts
@@ -1,37 +1,21 @@
-const http = require("http");
-function newGuid() {return ""};
-
-let i = 0;
-
-//import Forklift = require("./classes/Forklift.ts");
-import {Forklift} from "./classes/forklift"
 import {DataContainer} from "./classes/dataContainer"
 import {WebServer} from "./classes/webServer"
-
- 
-
-let data = new DataContainer();
-
-const handler = require("./handler")(data);
-const fs = require("fs");
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-
 class Main {
-    server
-    constructor(data, port, hostname) {
-        this.server = new WebServer(data, port, hostname);
+    server:WebServer;
+    data:DataContainer;
+    
+    constructor(port:number, hostname:string) {
+        this.data = new DataContainer();
+        this.server = new WebServer(this.data, port, hostname);
         this.server.run();
         this.update();
     }
-    
 
     update() {
-        console.log(`i: ${i++}`);
-        // Actual code
-
         let self = this;
         setImmediate(function() {
             self.update();
@@ -39,9 +23,4 @@ class Main {
     }
 }
 
-
-
-
-
-
-let main = new Main(data, port, hostname);
+let main = new Main(port, hostname);
