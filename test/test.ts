eba9152973b8abf2b8f5668491f5515736f59667
msan19 <msan19@student.aau.dk>
2020-03-31 13:50:12 +0200

    Added tester file

---
+++ test/test.ts
@@ -0,0 +1,12 @@
+import { helloTest } from '../src/hello-test';
+import { expect } from 'chai';
+import 'mocha';
+ 
+describe('First test', () => {
+ 
+  it('should return true', () => {
+    const result = helloTest();
+    expect(result).to.equal(true);
+  });
+ 
+});
