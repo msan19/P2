296fea27b15c9683b2191b935a50f11dbccae3d4
ChBLA <cbla19@student.aau.dk>
2020-03-31 14:37:44 +0200

    Test is now elephant

--- src/hello-test.ts
+++ src/hello-test.ts
@@ -1,3 +1,3 @@
 export function helloTest() {
-    return true;
+    return "elephant";
 }
--- test/test.ts
+++ test/test.ts
@@ -2,11 +2,13 @@ import { helloTest } from '../src/hello-test';
 import { expect } from 'chai';
 import 'mocha';
  
+const expected = "elephant";
+
 describe('First test', () => {
  
-  it('should return true', () => {
+  it(`should return ${expected}`, () => {
     const result = helloTest();
-    expect(result).to.equal(true);
+    expect(result).to.equal(expected);
   });
  
 });
