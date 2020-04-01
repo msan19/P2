923bf3978acccf029eacbcbd3a3a3f81e0deda8f
HGinnerup <hginnerup@hotmail.com>
2020-04-01 15:12:32 +0200

    Added websocket

--- src/planningScheduler/classes/webServer.ts
+++ src/planningScheduler/classes/webServer.ts
@@ -1,18 +1,26 @@
-import http = require("http");
-import fs = require("fs");
+import * as http from "http";
 import { Handler } from "./handler";
 import { DataContainer } from "./dataContainer";
+import * as WebSocket from "ws";
+import * as net from "net";
+import { Forklift } from "./forklift";
 
 export class WebServer {
     server: http.Server;
     port: number;
     hostname: string;
     handler: Handler;
+    webSocket: WebSocket.Server;
+
 
     constructor(data: DataContainer, port: number, hostname: string) {
         this.port = port;
         this.hostname = hostname;
         this.handler = new Handler(data);
+        this.webSocket = new WebSocket.Server({
+            noServer: true
+        });
+
         this.server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse): void => {
             request.method = request.method.toUpperCase();
             let parsedUrl = request.url.split("/");
@@ -27,8 +35,33 @@ export class WebServer {
                 response.writeHead(404);
                 response.end();
             }
+        });
+
+
+        this.server.on("upgrade", (request: http.IncomingMessage, socket: net.Socket, head: Buffer) => {
+            console.log("web-socket from: " + request.url)
+
+            if (true /* isForklift */) {
+                let id = "sfgsdafgdfs";
+
+
+                this.webSocket.handleUpgrade(request, socket, head, (ws: WebSocket) => {
+                    if (!data.forklifts[id]) {
+                        data.forklifts[id] = new Forklift(id);
+                    }
+                    data.forklifts[id].socket = ws;
+                    // ws.on('message', (message:WebSocket.Data) => {
+                    //   console.log('received: %s', message);
+                    // });
+
+                    ws.send('HTTP/1.1 101 Web Socket Protocol Handshake\r\n' +
+                        'Upgrade: WebSocket\r\n' +
+                        'Connection: Upgrade\r\n' +
+                        '\r\n');
+                });
             }
         });
+
     }
 
     run(): void {
