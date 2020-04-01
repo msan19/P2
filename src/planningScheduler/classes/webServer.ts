f7f5d96e803960d5efa65204719b809adf2e8d2f
HGinnerup <hginnerup@hotmail.com>
2020-04-01 13:10:26 +0200

    Moved Webserver.serverSetup into the constructor to maintain 'this'

--- src/planningScheduler/classes/webServer.ts
+++ src/planningScheduler/classes/webServer.ts
@@ -1,45 +1,45 @@
 import http = require("http");
 import fs = require("fs");
-import {Handler} from "./handler";
-import {DataContainer} from "./dataContainer";
+import { Handler } from "./handler";
+import { DataContainer } from "./dataContainer";
 
 export class WebServer {
-    server:http.Server;
-    port:number;
-    hostname:string;
-    handler:Handler;
-    constructor(data:DataContainer, port:number, hostname:string) {
+    server: http.Server;
+    port: number;
+    hostname: string;
+    handler: Handler;
+
+    constructor(data: DataContainer, port: number, hostname: string) {
         this.port = port;
         this.hostname = hostname;
         this.handler = new Handler(data);
-    }
-
-    serverSetup(request:http.IncomingMessage, response:http.ServerResponse): void {
-        request.method = request.method.toUpperCase();
-        let parsedUrl = request.url.split("/");
-        console.log(`${request.method}`);
-        // control sequence
-        switch (parsedUrl[1]) {
-            case "":
-                response.writeHead(200);
-                response.end(fs.readFileSync("tester.html"));
-            case "forklifts":
-                this.handler.forklifts(request, response, parsedUrl);
-                break;
-            case "routes":
-                this.handler.routes(request, response, parsedUrl);
-                break;
-            case "warehouse":
-                this.handler.warehouse(request, response, parsedUrl);
-                break;
-            case "orders":
-                this.handler.orders(request, response, parsedUrl);
-                break;
-            default:
-                response.writeHead(404);
-                response.end();
+        this.server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse): void => {
+            request.method = request.method.toUpperCase();
+            let parsedUrl = request.url.split("/");
+            console.log(`${request.method}`);
+            // control sequence
+            switch (parsedUrl[1]) {
+                case "":
+                    response.writeHead(200);
+                    response.end(fs.readFileSync("tester.html"));
+                case "forklifts":
+                    this.handler.forklifts(request, response, parsedUrl);
+                    break;
+                case "routes":
+                    this.handler.routes(request, response, parsedUrl);
+                    break;
+                case "warehouse":
+                    this.handler.warehouse(request, response, parsedUrl);
+                    break;
+                case "orders":
+                    this.handler.orders(request, response, parsedUrl);
+                    break;
+                default:
+                    response.writeHead(404);
+                    response.end();
                 // Error handling
-        }
+            }
+        });
     }
 
     run(): void {
