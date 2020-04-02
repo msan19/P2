76e7d59a500d61c70bc8cecef9ab1c3b02157a60
msan19 <msan19@student.aau.dk>
2020-04-02 15:51:37 +0200

    Removed WebClientHandler from server.ts

--- src/webClient/server.ts
+++ src/webClient/server.ts
@@ -1,10 +1,9 @@
 // TO DO 
 
 import { WebServerClient } from "../shared/webServer";
-import { WebClientHandler } from "./webClientHandler";
 
 let host = "127.0.0.1";
 let port = 8080;
 
-let server = new WebServerClient(WebClientHandler, port, host);
+let server = new WebServerClient(host, port);
 console.log(server);
