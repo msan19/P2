fd5343033c7991fc31c45a9301c003194ae37987
ChBLA <cbla19@student.aau.dk>
2020-04-02 10:47:14 +0200

    Formatting

--- test/planningScheduler/test_classes/testDataContainer.ts
+++ test/planningScheduler/test_classes/testDataContainer.ts
@@ -1,7 +1,7 @@
-import { DataContainer } from "./../../../src/planningScheduler/classes/dataContainer"
-import {Warehouse} from "./../../../src/planningScheduler/classes/warehouse";
-import {Order} from "./../../../src/planningScheduler/classes/order";
-import {Route} from "./../../../src/planningScheduler/classes/route";
+import { DataContainer } from "./../../../src/planningScheduler/classes/dataContainer";
+import { Warehouse } from "./../../../src/planningScheduler/classes/warehouse";
+import { Order } from "./../../../src/planningScheduler/classes/order";
+import { Route } from "./../../../src/planningScheduler/classes/route";
 import { expect } from 'chai';
 import 'mocha';
 
@@ -36,7 +36,7 @@ function testAddOrder() {
             expect(data.orders.length).to.equal(0);
         });
     });
-    
+
     // Adding an order
     describe(`When an order is added`, function () {
         let data = new DataContainer();
@@ -45,19 +45,19 @@ function testAddOrder() {
         it(`Order number 1's ${Object.keys(Order)[0]} should be ${Order.types.moveForklift}`, function () {
             expect(data.orders[0].type).to.equal(Order.types.moveForklift);
         });
-    
+
         it(`Order number 1's ${Object.keys(data.orders[0])[1]} should be "awd"`, function () {
             expect(data.orders[0].forkliftId).to.equal("awd");
         });
-    
+
         it(`Order number 1's ${Object.keys(data.orders[0])[2]} should be "pallet1"`, function () {
             expect(data.orders[0].palletId).to.equal("pallet1");
         });
-    
+
         it(`Order number 1's ${Object.keys(data.orders[0])[3]} should be "start1"`, function () {
             expect(data.orders[0].startVertexId).to.equal("start1");
         });
-    
+
         it(`Order number 1's ${Object.keys(data.orders[0])[4]} should be "slut1"`, function () {
             expect(data.orders[0].endVertexId).to.equal("slut1");
         });
