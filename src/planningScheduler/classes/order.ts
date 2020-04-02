0b54e0a4c0ec0e0973bf326b85bb97d1762b426d
HGinnerup <hginnerup@hotmail.com>
2020-04-02 09:49:30 +0200

    Export order.types

--- src/planningScheduler/classes/order.ts
+++ src/planningScheduler/classes/order.ts
@@ -1,23 +1,26 @@
 
-enum OrderType {
+export enum OrderTypes {
   movePallet = 1,
   moveForklift,
   charge,
 }
 
 export class Order {
-  type: OrderType;
+  static types = OrderTypes;
+  type: OrderTypes;
   forkliftId: string;
   palletId: string;
   startVertexId: string;
   endVertexId: string;
   
-  constructor(type: OrderType, forkliftId: string, palletId: string, startVertexId: string, endVertexId: string) {
+  constructor(type: OrderTypes, forkliftId: string, palletId: string, startVertexId: string, endVertexId: string) {
     this.type = type;
     this.forkliftId = forkliftId;
     this.palletId = palletId;
     this.startVertexId = startVertexId;
     this.endVertexId = endVertexId;
+
+    Order.types.moveForklift
   }
 
 }
