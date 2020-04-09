65af89a0436b86f3fc20ab8986813610945cd352
jamadaha <53914190+jamadaha@users.noreply.github.com>
2020-04-09 12:42:14 +0200

    Created getGraph function

--- src/webClient/scripts/sigma/graphManager.ts
+++ src/webClient/scripts/sigma/graphManager.ts
@@ -1,6 +1,20 @@
-sigma.parsers.json('tempData.JSON', {
-    container: 'sigma-container',
-    settings: {
-        defaultNodeColor: '#ec5148'
-    }
-});
+import { ApiCaller } from "../../../shared/apiCaller";
+let apiCaller = new ApiCaller("localhost", 3000);
+updateGraph();
+getGraph();
+
+function updateGraph(): void {
+    //@ts-ignore
+    sigma.parsers.json('tempData.JSON', {
+        container: 'sigma-container',
+        settings: {
+            defaultNodeColor: '#ec5148'
+        }
+    });
+}
+
+function getGraph(): void {
+    apiCaller.getWarehouse().then((warehouse) => {
+        console.log("Good!");
+    });
+}
