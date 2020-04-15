0bc49e6e93150958ccdcb7be9c058fb5aac1f986
ChBLA <cbla19@student.aau.dk>
2020-04-15 15:49:00 +0200

    More doc

--- src/planningScheduler/planningScheduler.ts
+++ src/planningScheduler/planningScheduler.ts
@@ -2,18 +2,20 @@ import { DataContainer } from "./classes/dataContainer";
 import { WebServerPlanningScheduler } from "./classes/WebServer";
 
 /**
- * Desc
+ * Object to handle event loop of main server.
+ * Object where main server is run from.
  */
 export class PlanningScheduler {
-    /** */
+    /** Server object to run the server from */
     server: WebServerPlanningScheduler;
-    /** */
+    /** Object to store data regarding warehouse, forklifts and orders */
     data: DataContainer;
 
     /**
-     * 
-     * @param port 
-     * @param hostname 
+     * Constructor for object.
+     * Initializes the server and starts the event loop using this.update() 
+     * @param port The port number associated with the server
+     * @param hostname The host name associated with the server
      */
     constructor(port: number, hostname: string) {
         this.data = new DataContainer();
@@ -23,7 +25,7 @@ export class PlanningScheduler {
     }
 
     /**
-     * 
+     * Adds itself to the event loop, but it does not block other events.
      */
     update() {
         let self = this;
