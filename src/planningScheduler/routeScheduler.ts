dcfe729ecb9261cdba4f4423ecf127e4c21db60e
msan19 <msan19@student.aau.dk>
2020-04-23 14:28:35 +0200

    Corrected call to upStacking

--- src/planningScheduler/routeScheduler.ts
+++ src/planningScheduler/routeScheduler.ts
@@ -179,13 +179,13 @@ export class RouteScheduler {
         }
 
         /// TO DO
-        // if (order.timeType === Order.timeTypes.start) {
-        //     this.upStacking(endVertex, order, "", this.data.warehouse.maxForkliftSpeed);
-        //     // Recursively stacking up
-        // } else if (order.timeType === Order.timeTypes.end) {
-        //     this.downStacking(endVertex, order, order.time, "", this.data.warehouse.maxForkliftSpeed);
-        //     // Recursively stacking down
-        // }
+        if (order.timeType === Order.timeTypes.start) {
+            this.upStacking(endVertex, order, forkliftId, null);
+            // Recursively stacking up
+        } else if (order.timeType === Order.timeTypes.end) {
+            // this.downStacking(endVertex, order, order.time, "", this.data.warehouse.maxForkliftSpeed);
+            // Recursively stacking down
+        }
 
         //this.printRoute(startVertex, endVertex);
         //console.log("\n");
