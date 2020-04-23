373a219782bfd48a5efe35c22f8712e65127481c
msan19 <msan19@student.aau.dk>
2020-04-23 14:27:04 +0200

    Now we check the length of the scheduleItem list

--- src/shared/graph.ts
+++ src/shared/graph.ts
@@ -276,7 +276,7 @@ export class Vertex {
             }
         }
 
-        if (this.scheduleItems[i].arrivalTimeCurrentVertex > time) {
+        if (this.scheduleItems.length !== 0 && this.scheduleItems[i].arrivalTimeCurrentVertex > time) {
             return j - 1;
         } else {
             return j;
