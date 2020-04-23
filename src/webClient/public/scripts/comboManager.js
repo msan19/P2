dc40696a121474bfd32802b819208a0b6ec64e75
jamadaha <53914190+jamadaha@users.noreply.github.com>
2020-04-23 14:55:06 +0200

    Added ability to send Time in add Order

--- src/webClient/public/scripts/comboManager.js
+++ src/webClient/public/scripts/comboManager.js
@@ -104,7 +104,7 @@ function updateSelectedForkliftInformationOnUI() {
         yPos.innerHTML = "...";
         let state = document.querySelector("#selectedForkliftState");
         state.innerHTML = "...";
-        onDeselectElementInRouteInSelectedForklift();
+        removeSelectedForkliftRouteOnUI();
     }
 
 }
@@ -114,6 +114,13 @@ function updateForkliftFocus(forklift) {
     document.querySelector("#currentForklift").innerHTML = `<h3>${forklift}</h3>`;
     document.querySelector("#forklift-list").value = forklift;
 }
+
+// Set dateInput to correct format
+$(function () {
+    $('#sendOrderDateTimePicker').datetimepicker();
+});
+
+
 
 // Graph events
 window.socketManager.on(PackageTypes.warehouse, (warehouse) => {
