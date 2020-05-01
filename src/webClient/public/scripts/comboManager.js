7c530b6e1e1bd63913d0a9b76189183aae391eec
jamadaha <53914190+jamadaha@users.noreply.github.com>
2020-05-01 10:59:48 +0200

    Forklifts can now move further then to the nex Node

--- src/webClient/public/scripts/comboManager.js
+++ src/webClient/public/scripts/comboManager.js
@@ -1,7 +1,7 @@
 window.nForklifts = new Forklifts(null);
 window.mainGraph;
 
-window.frameRate = 120;
+window.frameRate = 1;
 
 var forkliftData = [];
 var ForkliftStates;
@@ -216,6 +216,7 @@ window.socketManager.on(PackageTypes.route, (route) => {
 // Event loop
 window.setInterval(function () {
     if (typeof (mainGraph) != "undefined") {
+        nForklifts.addTestDataToForklifts();
         nForklifts.handleForkliftMovement();
         updateSelectedForkliftInformationOnUI();
         mainGraph.updateForkliftsOnGraph();
