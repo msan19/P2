67f563f1b9166008788b114168a57f42c68be161
ChBLA <cbla19@student.aau.dk>
2020-05-07 13:43:56 +0200

    Reworked test of warehouse in planningScheduler

--- test/planningScheduler/test_classes/testWarehouse.ts
+++ test/planningScheduler/test_classes/testWarehouse.ts
@@ -1,121 +1,62 @@
 // testAstar.ts
 /**
- * Test of ({@link Warehouse})
+ * Test of ({@link Warehouse}). Assumes 
  * @packageDocumentation
  * @category planningScheduler test
  */
 
 import { expect } from 'chai';
 import 'mocha';
-import { Warehouse } from '../../../src/shared/warehouse';
-import { Graph, Vertex } from '../../../src/shared/graph';
+import { Warehouse } from '../../../src/planningScheduler/classes/warehouse';
 import { Vector2 } from '../../../src/shared/vector2';
+import { Graph, Vertex } from '../../../src/planningScheduler/classes/graph';
 
-/** 
- * Considers whether two warehouses contains same values using Mocha
- * @param result The warehouse to be checked
- * @param expected The expected warehouse
- * @returns Mocha handles the appropriate responses
- */
-function checkWarehouse(result: any, expected: Warehouse | null): void {
-    if (expected !== null) {
-        it(`Speed should be ${expected.maxForkliftSpeed}`, () => {
-            expect(result.maxForkliftSpeed).to.equal(expected.maxForkliftSpeed);
-        });
-        checkArray(Object.keys(result.graph.vertices), Object.keys(expected.graph.vertices));
-    } else {
-        it(`Should be ${expected}`, () => {
-            expect(result).to.equal(expected);
-        });
-    }
-}
-
-/**
- * Checks whether two arrays cointains same values using Mocha
- * @param result The array to be checked
- * @param expected The array containing the expected values
- * @returns Mocha handles the appropriate responses
- */
-function checkArray(result: string[], expected: string[]): void {
-    let length: number = Math.max(result.length, expected.length);
-    for (let i = 0; i < length; i++) {
-        it(`${result[i]} should be ${expected[i]}`, () => {
-            expect(result[i]).to.equal(expected[i]);
-        });
-    }
-}
-
-/**
- * Test of the object Warehouse
- * @returns Mocha handles the appropriate responses
- */
-function testWarehouse(): void {
-    let graph: Graph = new Graph({
+describe(`Warehouse parse`, () => {
+    let originalGraph = new Graph({
         "N23": new Vertex("N23", new Vector2(10, 10)),
-        "N27": new Vertex("N27", new Vector2(20, 20)),
-        "N29": new Vertex("N29", new Vector2(30, 30))
+        "N27": new Vertex("N27", new Vector2(20, 20))
     });
-    let warehouse: Warehouse = new Warehouse(graph, 20);
-    let expectedArray: string[] = ["N23", "N27", "N29"];
-    let expectedSpeed: number = 20;
+    let originalMaxForkliftSpeed = 15;
 
-    it(`ForkliftSpeed should be ${expectedSpeed}`, () => {
-        expect(warehouse.maxForkliftSpeed).to.equal(expectedSpeed);
-    });
-
-    checkArray(Object.keys(warehouse.graph.vertices), expectedArray);
-}
-
-/**
- * Test of parse method on Warehouse object
- * @returns Mocha handles the appropriate responses
- */
-function testParse(): void {
-    let graph: Graph = new Graph({
+    let warehouseReference = new Warehouse(new Graph({
         "N23": new Vertex("N23", new Vector2(10, 10)),
-        "N27": new Vertex("N27", new Vector2(20, 20)),
-        "N29": new Vertex("N29", new Vector2(30, 30))
+        "N27": new Vertex("N27", new Vector2(20, 20))
+    }), 15);
+
+    context(`when valid graph, valid maxForkliftSpeed`, () => {
+        let testObject = { graph: originalGraph, maxForkliftSpeed: originalMaxForkliftSpeed };
+        let resultingWarehouse = Warehouse.parse(testObject);
+        let expectedWarehouse = warehouseReference;
+        it(`should be parsed as a warehouse`, () => {
+            expect(resultingWarehouse).to.eql(expectedWarehouse);
+        });
     });
 
-    describe(`Valid warehouse`, () => {
-        let warehouse: Warehouse = new Warehouse(graph, 20);
-        let expected: Warehouse = new Warehouse(graph, 20);
-        let result: Warehouse | null = Warehouse.parse(warehouse);
-
-        checkWarehouse(result, expected);
+    context(`when valid graph, invalid maxForkliftSpeed`, () => {
+        let testObject = { graph: originalGraph, maxForkliftSpeed: null };
+        let resultingWarehouse = Warehouse.parse(testObject);
+        let expectedWarehouse = null;
+        it(`should not be parsed as a warehouse`, () => {
+            expect(resultingWarehouse).to.eql(expectedWarehouse);
+        });
     });
 
-    describe(`Invalid vertex in graph from warehouse`, () => {
-        let warehouse: Warehouse = new Warehouse(graph, 20);
-        warehouse.graph.vertices["N23"].id = null;
-        // graph.vertices["N23"] = null;
-        let expected: Warehouse = new Warehouse(graph, 20);
-        let result: Warehouse | null = Warehouse.parse(warehouse);
-
-        checkWarehouse(result, expected);
-
+    context(`when invalid graph, valid maxForkliftSpeed`, () => {
+        let testObject = { graph: null, maxForkliftSpeed: originalMaxForkliftSpeed };
+        let resultingWarehouse = Warehouse.parse(testObject);
+        let expectedWarehouse = null;
+        it(`should not be parsed as a warehouse`, () => {
+            expect(resultingWarehouse).to.eql(expectedWarehouse);
+        });
     });
 
-    describe(`Invalid forkliftSpeed from warehouse`, () => {
-        let warehouse: Warehouse = new Warehouse(graph, 20);
-        warehouse.maxForkliftSpeed = null;
-
-        let expected: Warehouse | null = null;
-        let result: Warehouse | null = Warehouse.parse(warehouse);
-
-        checkWarehouse(result, expected);
+    context(`when invalid graph, invalid maxForkliftSpeed`, () => {
+        let testObject = { graph: null, maxForkliftSpeed: null };
+        let resultingWarehouse = Warehouse.parse(testObject);
+        let expectedWarehouse = null;
+        it(`should not be parsed as a warehouse`, () => {
+            expect(resultingWarehouse).to.eql(expectedWarehouse);
+        });
     });
 
-    describe(`Invalid graph from warehouse`, () => {
-        let warehouse: Warehouse = new Warehouse(graph, 20);
-        warehouse.graph = null;
-
-        let expected: Warehouse | null = null;
-        let result: Warehouse | null = Warehouse.parse(warehouse);
-
-        checkWarehouse(result, expected);
-    });
-}
-
-describe(`Test of Warehouse Object`, testWarehouse);
-describe(`Test of Warehouse Parse`, testParse);
+});
