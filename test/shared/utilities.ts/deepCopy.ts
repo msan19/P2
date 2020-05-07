ec134bb897b4aae81d3f43c1c668527673f95fa0
HGinnerup <hginnerup@hotmail.com>
2020-05-07 09:46:10 +0200

    deepCopy now adds references to copied objects, rather than the originals

--- test/shared/utilities.ts/deepCopy.ts
+++ test/shared/utilities.ts/deepCopy.ts
@@ -41,20 +41,26 @@ describe("deepCopy", () => {
         expect(copiedObj.arrayOfVectors[0].add(new Vector2(2, 2))).to.eql(new Vector2(3, 4)); // Try a function from prototype
     });
     it("deepCopy should do circular references, without crashing", () => {
-        let obj1 = {};
-        let obj2 = {};
-        obj1["obj2"] = obj2;
-        obj2["obj1"] = obj1;
+        // create circular reference
+        let obj1: { obj2: any; } = { obj2: null };
+        let obj2: { obj1: any; } = { obj1: null };
+        obj1.obj2 = obj2;
+        obj2.obj1 = obj1;
 
-        let copy;
-        try {
-            copy = deepCopy(obj1);
-            expect(copy.obj2).to.equal(copy.obj2.obj1.obj2.obj1.obj2);
-        } catch (e) {
-            console.error(e);
-        } finally {
-            expect(copy, "crashed from circular definition").to.exist;
-        }
+        // Copy circular reference
+        let copy = deepCopy(obj1);
+
+        // Test circular reference
+        expect(copy, "crashed from circular definition").to.exist;
+        expect(copy.obj2, "Referenced to original object").to.not.equal(obj1.obj2.obj1.obj2.obj1.obj2);
+
+        // Properties set on copy should not be on original
+        copy.obj2.obj1.obj2.a = "a";
+        expect(obj2["a"], "Referenced to original object").to.not.exist;
+        copy.obj2.obj1.obj2.obj1.b = "b";
+        expect(obj1["b"], "Referenced to original object").to.not.exist;
+
+        expect(copy.obj2, "obj2 changes throughout the chain").to.be.equal(copy.obj2.obj1.obj2.obj1.obj2);
 
     });
 });
