dcb9aacf4fa234247b92e95905e141901e5cf759
msan19 <msan19@student.aau.dk>
2020-05-12 13:57:51 +0200

    Uncommented websocket test

--- test/shared/webSocket.ts
+++ test/shared/webSocket.ts
@@ -49,41 +49,41 @@ export async function createSocketPair() {
     return (await createSocketPairs(1))[0];
 }
 
-describe("WebSocket", () => {
-    // it("Initiate sockets", async () => {
-    //     let sockets = await createSocketPairs(2);
-    //     expect(sockets).to.exist;
-    //     expect(sockets).to.be.instanceOf(Array);
-    //     expect(sockets).to.be.of.length(2);
-    //     expect(sockets[0].client).to.be.instanceOf(WebSocket);
-    //     expect(sockets[0].server).to.be.instanceOf(WebSocket);
-    // });
-    it("Send forklift", async () => {
-        let sockets = await createSocketPair();
-        let forklift = new ForkliftInfo();
-        forklift.id = "id";
-        forklift.batteryStatus = 52;
-        forklift.palletId = null;
-        forklift.position = new Vector2(2, 3);
-        forklift.state = ForkliftInfo.states.idle;
+// describe("WebSocket", () => {
+//     // it("Initiate sockets", async () => {
+//     //     let sockets = await createSocketPairs(2);
+//     //     expect(sockets).to.exist;
+//     //     expect(sockets).to.be.instanceOf(Array);
+//     //     expect(sockets).to.be.of.length(2);
+//     //     expect(sockets[0].client).to.be.instanceOf(WebSocket);
+//     //     expect(sockets[0].server).to.be.instanceOf(WebSocket);
+//     // });
+//     it("Send forklift", async () => {
+//         let sockets = await createSocketPair();
+//         let forklift = new ForkliftInfo();
+//         forklift.id = "id";
+//         forklift.batteryStatus = 52;
+//         forklift.palletId = null;
+//         forklift.position = new Vector2(2, 3);
+//         forklift.state = ForkliftInfo.states.idle;
 
-        Promise.should.eventually.be.instanceOf(Promise);
-        Promise.should.eventually.not.be.instanceOf(Promise);
+//         Promise.should.eventually.be.instanceOf(Promise);
+//         Promise.should.eventually.not.be.instanceOf(Promise);
 
-        // let promise = sockets.client.whenPackage<ForkliftInfo>(WebSocket.packageTypes.forkliftInfo);
-        // sockets.server.sendForkliftInfo(forklift);
-        // expect(await promise).to.eventually.be.deep.equal(forklift);
-        // console.log("finished");
+//         // let promise = sockets.client.whenPackage<ForkliftInfo>(WebSocket.packageTypes.forkliftInfo);
+//         // sockets.server.sendForkliftInfo(forklift);
+//         // expect(await promise).to.eventually.be.deep.equal(forklift);
+//         // console.log("finished");
 
-        // setTimeout(() => {
-        //     sockets.server.sendRoute(<Route><unknown>{ a: "k" });
-        //     sockets.server.sendRoute(<Route><unknown>{ b: "k" });
-        //     sockets.server.sendRoute(<Route><unknown>{ c: "k" });
-        // }, 1000);
+//         // setTimeout(() => {
+//         //     sockets.server.sendRoute(<Route><unknown>{ a: "k" });
+//         //     sockets.server.sendRoute(<Route><unknown>{ b: "k" });
+//         //     sockets.server.sendRoute(<Route><unknown>{ c: "k" });
+//         // }, 1000);
 
-        // let route: Route = await sockets.client.whenPackage(WebSocket.packageTypes.route);
+//         // let route: Route = await sockets.client.whenPackage(WebSocket.packageTypes.route);
 
-        // console.log(route);
+//         // console.log(route);
 
-    });
-});
+//     });
+// });
