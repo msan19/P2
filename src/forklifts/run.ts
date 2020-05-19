854db4e0274e00a274c2512030e5155ad604029f
Henrik Ginnerup <hginnerup@hotmail.com>
2020-05-19 13:21:35 +0200

    Switched forklifts to initiate 150 at random vertices

--- src/forklifts/run.ts
+++ src/forklifts/run.ts
@@ -6,7 +6,7 @@
  */
 
 import { Forklift } from "./forklift";
-import { randomIntegerInRange, randomValue } from "../shared/utilities";
+import { randomIntegerInRange, randomKey } from "../shared/utilities";
 import { ApiCaller } from "../shared/apiCaller";
 import { Warehouse } from "../shared/warehouse";
 import { Vector2 } from "../shared/vector2";
@@ -38,8 +38,21 @@ function getWarehouse() {
 
 
 getWarehouse().then((warehouse: Warehouse) => {
+    let verticeIds = Object.keys(warehouse.graph.vertices);
+    for (let i = 0; i < 150; i++) {
+        if (Object.keys(verticeIds).length <= 0) break; // empty spaces in array still counts in length, but not in keys
+
+        let randomId = randomKey(verticeIds);
+        let vertex = warehouse.graph.vertices[verticeIds[randomId]];
+        delete verticeIds[randomId];
+
+
+        forklifts.push(new Forklift("F" + i, SERVER_HOSTNAME, SERVER_PORT, randomIntegerInRange(5, 100), Forklift.states.idle, vertex.position));
+    }
+    /*
     for (let i = 0; i < 10; i++) {
         forklifts.push(new Forklift("F" + i, SERVER_HOSTNAME, SERVER_PORT, randomIntegerInRange(5, 100), Forklift.states.idle,
             new Vector2(i, 9 - i).scale(10)));
     }
+    */
 });
