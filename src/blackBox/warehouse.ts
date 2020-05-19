3ea34943d846792298452fdf9aec3dbeef828c52
ChBLA <cbla19@student.aau.dk>
2020-05-19 13:22:59 +0200

    Made the kiva-like experiment from Sven Koenig

--- src/blackBox/warehouse.ts
+++ src/blackBox/warehouse.ts
@@ -11,7 +11,7 @@ import { Vector2 } from "../shared/vector2";
  * Creates a graph object
  */
 export function createGraph() {
-    return new Graph(createVertices(10, 10));
+    return new Graph(createKivaLikeGraph(53, 22));
 }
 
 /**
@@ -39,6 +39,55 @@ function createVertices(xSize: number, ySize: number): { [key: string]: Vertex; 
                 let neighborId = `N${x - 1}-${y}`;
                 vertices[id].adjacentVertexIds.push(neighborId);
                 vertices[neighborId].adjacentVertexIds.push(id);
+            }
+        }
+    }
+    return vertices;
+}
+
+/**
+ * The total amount of generated vertices is xSize * ySize
+ * @param xSize Amount of vertices on the x axis
+ * @param ySize Amount of vertices on the y axis
+ * @return { [key: string]: Vertex; } A dictionary where the values are Vertex objects
+ */
+function createKivaLikeGraph(xSize: number, ySize: number): { [key: string]: Vertex; } {
+    let vertices = {};
+    let distanceOfEdges = 10;
+    let boundaryX = { left: 5, right: xSize - 5 };
+
+
+    for (let x = 0; x < xSize; x++) {
+        for (let y = 0; y < ySize; y++) {
+            if (x >= boundaryX.left && x < boundaryX.right && ((x - 5) % 11 === 10 || y % 3 === 0)) {
+                let id = `N${x}-${y}`;
+                vertices[id] = new Vertex(id, new Vector2(x * distanceOfEdges, y * distanceOfEdges), id);
+
+                if (y > 0 && (x - 5) % 11 === 10) {
+                    let neighborId = `N${x}-${y - 1}`;
+                    vertices[id].adjacentVertexIds.push(neighborId);
+                    vertices[neighborId].adjacentVertexIds.push(id);
+                }
+                if (x > 0 && y % 3 === 0) {
+                    let neighborId = `N${x - 1}-${y}`;
+                    vertices[id].adjacentVertexIds.push(neighborId);
+                    vertices[neighborId].adjacentVertexIds.push(id);
+                }
+
+            } else if (x < boundaryX.left || x >= boundaryX.right) {
+                let id = `N${x}-${y}`;
+                vertices[id] = new Vertex(id, new Vector2(x * distanceOfEdges, y * distanceOfEdges), id);
+                if (y > 0) {
+                    let neighborId = `N${x}-${y - 1}`;
+                    vertices[id].adjacentVertexIds.push(neighborId);
+                    vertices[neighborId].adjacentVertexIds.push(id);
+                }
+                if (x > 0 && (x > boundaryX.right || x < boundaryX.left || y % 3 === 0)) {
+                    let neighborId = `N${x - 1}-${y}`;
+                    vertices[id].adjacentVertexIds.push(neighborId);
+                    vertices[neighborId].adjacentVertexIds.push(id);
+                }
+
             }
         }
     }
