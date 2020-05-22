2785eb3277285c4090e88151f9d97c3ffb13ffae
jamadaha <53914190+jamadaha@users.noreply.github.com>
2020-05-22 09:52:12 +0200

    Now prepends initial to all Graph elements on the Sigma Graph

--- src/webClient/public/scripts/Route.js
+++ src/webClient/public/scripts/Route.js
@@ -42,7 +42,7 @@ class Route {
         let instructions = [];
         for (let key in data.instructions) {
             instructions.push({
-                nodeId: data.instructions[key].vertexId,
+                nodeId: Graph.prepareNodeId(data.instructions[key].vertexId),
                 startTime: data.instructions[key].startTime
             });
         }
